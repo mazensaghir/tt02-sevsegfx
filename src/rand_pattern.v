a61cfe23c24d6468c7c0830dd04905e5f4830c47
Mazen Saghir <mazen.saghir@gmail.com>
2022-12-03 18:19:53 +0200

    Update rand_pattern.v

--- src/rand_pattern.v
+++ src/rand_pattern.v
@@ -6,15 +6,15 @@ module rand_pattern
 
   always@(i_count) begin
     case(i_count)
-      0 : o_segment <= 7'b0000010;
-      1 : o_segment <= 7'b1000000;
-      2 : o_segment <= 7'b0000001;
-      3 : o_segment <= 7'b0010000;
-      4 : o_segment <= 7'b0100000;
-      5 : o_segment <= 7'b0001000;
-      6 : o_segment <= 7'b0000100;
-      7 : o_segment <= 7'b0100000;
-      default : o_segment <= 7'b0000000;
+      0 : o_segment = 7'b0000010;
+      1 : o_segment = 7'b1000000;
+      2 : o_segment = 7'b0000001;
+      3 : o_segment = 7'b0010000;
+      4 : o_segment = 7'b0100000;
+      5 : o_segment = 7'b0001000;
+      6 : o_segment = 7'b0000100;
+      7 : o_segment = 7'b0100000;
+      default : o_segment = 7'b0000000;
     endcase
   end
 
